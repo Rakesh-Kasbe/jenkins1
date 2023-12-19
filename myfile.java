f126b06d102f35a332b27cd3857d3f27649230a5
root <root@ip-172-31-10-90.ap-south-1.compute.internal>
2023-12-19 09:30:19 +0000

    repot

--- myfile.java
+++ myfile.java
@@ -1,7 +1,7 @@
 
     class Simple{  
         public static void main(String args[]){  
-         System.out.println("Welcome to My Project");  
+         System.out.println("Welcome to My Trigger Project");  
         }  
     }  
 
